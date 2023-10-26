e120de658613d3af7b77fd019f87512b41ed63a8
Githinji <githinji.ngare@gmail.com>
2023-10-26 21:06:40 +0300

    sign-up build with integrity error issue

--- server/config.py
+++ server/config.py
@@ -2,11 +2,13 @@ from flask import Flask
 from flask_restful import Api
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
 # app.config['SQLALCHEMY_DATABASE_URI'] = '(postgres link goes here)'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'your_secret_key_here'
 app.json.compact = False
 
 db = SQLAlchemy()
@@ -14,4 +16,6 @@ migrate = Migrate(app, db)
 db.init_app(app)
 
 api = Api(app)
+csrf = CSRFProtect(app)
+
 
---
+++ server/forms.py
@@ -0,0 +1,17 @@
+# forms.py
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField,SelectField
+from wtforms.validators import DataRequired,EqualTo,Email
+
+class SignUpForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    role = SelectField('Role', choices=[('user', 'User'), ('admin', 'Admin')], validators=[DataRequired()])
+    submit = SubmitField('Sign Up')
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Log In')
