8e6d9146fba0c74f426d1d3b14a2f9228f34a72d
whiplade <nmawira13@gmail.com>
2023-10-26 17:01:06 +0300

    prevention of circular imports

---
+++ server/config.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_restful import Api
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+# app.config['SQLALCHEMY_DATABASE_URI'] = '(postgres link goes here)'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
+
+db = SQLAlchemy()
+migrate = Migrate(app, db)
+db.init_app(app)
+
+api = Api(app)
+
