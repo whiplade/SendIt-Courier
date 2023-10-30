eca39303d567ff04c066ca77e953e977e257072c
vivian waithera <vivyannekori@gmail.com>
2023-10-30 16:53:10 +0300

    Login changes

--- server/config.py
+++ server/config.py
@@ -4,6 +4,7 @@ from flask_restful import Api
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://whiplade:EQO9vizVeJPWblgTzx3UcdLKRKDqkQ5j@dpg-cktmdvunfb1c73f5kk60-a.singapore-postgres.render.com/sendit_fvf8'
@@ -19,6 +20,8 @@ migrate = Migrate(app, db)
 db.init_app(app)
 
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
 
 password = "user_password"
 hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
