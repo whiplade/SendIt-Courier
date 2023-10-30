49df908f15e7b2520ac1b80b5736b6209797d15b
vivian waithera <vivyannekori@gmail.com>
2023-10-30 11:32:08 +0300

    conflicts resolve

--- server/config.py
+++ server/config.py
@@ -6,12 +6,12 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://whiplade:EQO9vizVeJPWblgTzx3UcdLKRKDqkQ5j@dpg-cktmdvunfb1c73f5kk60-a.singapore-postgres.render.com/sendit_fvf8'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://whiplade:EQO9vizVeJPWblgTzx3UcdLKRKDqkQ5j@dpg-cktmdvunfb1c73f5kk60-a.singapore-postgres.render.com/sendit_fvf8'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'your_secret_key_here'
 app.json.compact = False
-app.secret_key = b'#^yiu{/}]`poier343ufe`'
+app.secret_key = '#^yiu{/}]`poier343ufe`'
 
 
 db = SQLAlchemy()
@@ -19,6 +19,19 @@ migrate = Migrate(app, db)
 db.init_app(app)
 
 bcrypt = Bcrypt(app)
+
+password = "user_password"
+hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
+
+
+password = "user_password"  
+hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
+
+password_entered_by_user = "user_password"  
+if bcrypt.check_password_hash(hashed_password, password_entered_by_user):
+    print("Login successful")
+else:
+    print("Invalid password")
 
 api = Api(app)
 csrf = CSRFProtect(app)
