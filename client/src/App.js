29e2195bfe0cb0766ae4545f15577fb7f2582632
Drake Tempes <dreake.ntimama@student.moringaschool.com>
2023-11-02 11:05:37 +0300

    Message

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import './App.css';
 import About from './components/Landing/About';
 import Pricing from './components/Landing/Pricing';
 import Login from './components/Landing/Login';
-import SignUp from './components/Landing/Signup';
+import SignUp from './components/Landing/SignUp';
 import Landing from './components/Landing/Landing';
 import User from './components/User/User';
 import Admin from './components/Admin/Admin';
